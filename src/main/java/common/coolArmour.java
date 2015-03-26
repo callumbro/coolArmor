5e51918fb8d62b925953074d7fa01b332ecfdb1f
Callum Johnson <callumbro@hotmail.com>
2015-03-26 12:20:29 -0400

    first

---
+++ src/main/java/common/coolArmour.java
@@ -0,0 +1,15 @@
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Callum on 02/27/2015.
+ */
+
+@Mod(modid = coolArmour.MODID, version = coolArmour.VERSION)
+public class coolArmour
+{
+    public static final String MODID = "Cool Armour";
+    public static final String VERSION = "1.0";
+
+
+
+}
